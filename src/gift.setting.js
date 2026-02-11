70c8a307e71bdabd7836bcfc65b879ff96cf6561
mtclllin <152270196+michelin-5star@users.noreply.github.com>
2026-02-10 19:17:37 -0800

    Update gift.setting.js

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -11,15 +11,15 @@ export const IS_SHOW_RESTART = false;
 // 首页配置
 export const homeConfig = {
   // 标题
-  title: '520送礼抽奖',
+  title: '老婆咪的Valentine小礼物抽奖～',
   // 留言，数组的每一项代表一行
   messages: [
-    { key: 'a', wording: '这是第一行文案', },
-    { key: 's', wording: '这是第二行文案', },
-    { key: 'd', wording: '然后，这里是第三行文案', },
+    { key: 'a', wording: '骚瑞老公情人节不能陪在老婆身边，但是老公特别想你特别爱你的！', },
+    { key: 's', wording: '所以请老婆抽个小奖以作为延期约会的补偿', },
+    { key: 'd', wording: 'Will you be my Valentine？好嘛好嘛', },
   ],
   // 最终解释权归属人
-  owner: 'XXX',
+  owner: '老公宝包',
   // 抽奖转盘的动画时间，单位毫秒
   timeout: 5000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
@@ -31,12 +31,12 @@ export const homeConfig = {
 // 图片放在 public/images 中，建议是正方形，否则影响视觉体验
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
-  { key: 'q', name: 'Dior双飞套装', alias: 'Dior双飞', image: '/images/1.png',  description: '「经典迷人色彩，彰显奢华魅力」' },
-  { key: 'w', name: '腾讯视频终身VIP', alias: 'VIP', image: '/images/2.png',  description: '“为你承包一辈子的 VIP”' },
-  { key: 'e', name: '兰蔻小黑瓶套装', alias: '小黑瓶套装', image: '/images/3.png',  description: '「强维稳，快修护」' },
-  { key: 'r', name: '520红包', alias: '520', image: '/images/6.png',  description: '“想给你唱一百首情歌”' },
-  { key: 't', name: 'Dior星空套装', alias: 'Dior星空', image: '/images/5.png',  description: '「百变唇妆，精美雕琢」' },
-  { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“从今往后，我都会在你旁边”' },
-  { key: 'u', name: 'UR购物券：¥1,000', alias: 'UR', image: '/images/7.png',  description: '“UR！买！”' },
-  { key: 'i', name: 'Dyson美发套装', alias: '戴森', image: '/images/8.png',  description: '「不同造型需求，全面满足」' },
+  { key: 'q', name: 'Meshki/PPMY裙裙一件', alias: '裙裙', image: '/images/1.png',  description: '「老婆喜欢漂亮裙裙」' },
+  { key: 'w', name: '老公的一百个亲亲券', alias: '亲亲', image: '/images/2.png',  description: '“老公亲亲！”' },
+  { key: 'e', name: 'Sephora购物一次', alias: 'Sephora', image: '/images/3.png',  description: '「美美老婆～」' },
+  { key: 'r', name: '北加州getaway trip一次', alias: '旅行～', image: '/images/6.png',  description: '“人称小bora bora”' },
+  { key: 't', name: '豪华晚餐约会+1', alias: '约会～', image: '/images/5.png',  description: '「吃大肉噜！！！」' },
+  { key: 'y', name: '再买一份花花～', alias: '花花', image: '/images/6.png',  description: '“给老婆订漂亮花花～不算情人节的哦”' },
+  { key: 'u', name: 'Rhode最新单品一份', alias: 'Rhode', image: '/images/7.png',  description: '“即便老婆已经有8个一模一样的蛋噜”' },
+  { key: 'i', name: '钻石一颗', alias: '钻石', image: '/images/8.png',  description: '「一颗恒久远～给老婆做项链（先搞个小一点的不要让老公破产捏）」' },
 ];
